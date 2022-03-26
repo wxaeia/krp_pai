fa43a5656c8fc3d7ba3d7ae1c05a02ee2d17c220
Rishikesh2790 <rushi.s.patil6@gmail.com>
2022-03-26 23:04:19 +0530

    added secret key

--- krp_pai/settings.py
+++ krp_pai/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = "django-insecure-n39%hrbd-wo%fhl+qlc0(kvk^#00lkpi&g@-*haod3)ql$1eo*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
