696a8c403adcdb1517b346df6335f5083c6ca89d
Rishikesh2790 <rushi.s.patil6@gmail.com>
2022-03-27 01:51:28 +0530

    code

--- krp_pai/settings.py
+++ krp_pai/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'krp_pai.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'database',
+        'USER': 'postgre',
+        'PASSWORD': 'pass12345',
+        'HOST': 'database.cmxyo7dc4thg.ap-south-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
