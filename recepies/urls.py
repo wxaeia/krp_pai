cd55a0774646394aa023c7cfd04cbc2acf133973
Rishikesh2790 <rushi.s.patil6@gmail.com>
2022-03-26 23:45:29 +0530

    code

--- recepies/urls.py
+++ recepies/urls.py
@@ -2,7 +2,7 @@
 from rest_framework import routers
 from recepies.views import ReceipeViewSet
 router = routers.SimpleRouter()
-router.register(r'receipe', ReceipeViewSet)
+router.register(r'', ReceipeViewSet)
 # router.register(r'accounts', AccountViewSet)
 
 urlpatterns = [
