d134facf86742ae5a5d3a7ad800815ccd60acd26
wxaeia <59302650+wxaeia@users.noreply.github.com>
2022-03-28 16:22:54 +0530

    code

--- krp_pai/settings.py
+++ krp_pai/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-n39%hrbd-wo%fhl+qlc0(kvk^#00lkpi&g@-*haod3)ql$1eo*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
