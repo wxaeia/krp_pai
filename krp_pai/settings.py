327d15f83de91a25863ff2102a043681849aed17
Rishikesh2790 <rushi.s.patil6@gmail.com>
2022-03-28 16:19:06 +0530

    static root url

--- krp_pai/settings.py
+++ krp_pai/settings.py
@@ -148,4 +148,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = BASE_DIR /'static/'
